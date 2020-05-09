00b3c1b06f8fb195883e5558cb875628d60d86cc
XarenySC <xarenysc@gmail.com>
2020-05-09 16:12:42 -0500

    modificacion estilos

--- pages/Test.js
+++ pages/Test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Layout from '../Components/Layout'
 const Test = () => {
-    return(
-        <Layout>
-            <h1>Test</h1>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <h1>Test</h1>
+    </Layout>
+  )
 }
 
 
